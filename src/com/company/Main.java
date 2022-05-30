eb49dda9d194e090be13a08a43554369d2856aaa
alekseytumbaev <leha.tumbaev@gmail.com>
2022-05-30 09:50:45 +0700

    Module 1. Load city data from file, convert to list of cities and output to console

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,48 @@
 package com.company;
 
+import com.company.models.City;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        //создаем scanner и передаем ему в конструктор путь файла с городами
+        try(Scanner scanner = new Scanner(Paths.get(
+                "src\\com\\company\\data\\city_ru.csv"))) {
+
+            List<City> cities = new ArrayList<>();
+
+            //передаем сканнеру системный разделитель строк
+            scanner.useDelimiter(System.getProperty("line.separator"));
+
+            //пока в файле есть строки
+            for(int i = 0; scanner.hasNext(); i++){
+                cities.add(parseCityCSVLine(scanner.next())); //парсим строку в город и добавляем в список
+                System.out.println(cities.get(i).toString()); //выводим очередной город
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //метод преобразования строк в город
+    private static City parseCityCSVLine(String line){
+        Scanner scanner = new Scanner(line);
+        scanner.useDelimiter(";");
+        scanner.next(); //пропускаем номер города в файле
+
+        String name = scanner.next();
+        String region = scanner.next();
+        String district = scanner.next();
+        int population = scanner.nextInt();
+        String foundation = scanner.hasNext() ? scanner.next() : null; //не для всех городов известен год основания
+
+        return new City(name,region,district,population,foundation);
     }
 }
---
+++ src/com/company/models/City.java
@@ -0,0 +1,71 @@
+package com.company.models;
+
+public class City {
+    private String name;
+    private String region;
+    private String district;
+    private int population;
+    private String foundation;
+
+    public City(String name, String region, String district, int population, String foundation) {
+        this.name = name;
+        this.region = region;
+        this.district = district;
+        this.population = population;
+        this.foundation = foundation;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", region='" + region + '\'' +
+                ", district='" + district + '\'' +
+                ", population=" + population +
+                ", foundation='" + foundation + '\'' +
+                '}';
+    }
+
+    public City() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public String getFoundation() {
+        return foundation;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public void setFoundation(String foundation) {
+        this.foundation = foundation;
+    }
+}
