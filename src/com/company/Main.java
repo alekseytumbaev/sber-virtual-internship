2a0805da5daf38d48895231e136f646341971f05
alekseytumbaev <leha.tumbaev@gmail.com>
2022-06-03 10:56:54 +0700

    Module 3 improved. Add merge sort and quick sort methods for founding most populous city.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,8 +13,15 @@ public class Main {
             CityRepository cityRepository = new CityRepository("src\\resources\\city_ru.csv");
             CityService cityService = new CityService(cityRepository);
 
-            System.out.println("Most populous city index and population");
-            cityService.printMostPopulousCityIndexAndPopulation();
+            System.out.println("Most populous city index and population found by brute force");
+            cityService.printHighestPopulationBruteForce();
+
+            System.out.println("Most populous city index and population found by merge sort");
+            cityService.printHighestPopulationMergeSort();
+
+            System.out.println("Most populous city index and population found by quick sort");
+            cityService.printHighestPopulationQuickSort();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/com/company/services/CityService.java
+++ src/com/company/services/CityService.java
@@ -3,6 +3,9 @@ package com.company.services;
 import com.company.models.City;
 import com.company.repositories.CityRepository;
 
+import java.text.MessageFormat;
+import java.util.Random;
+
 /**
  * Класс, совершающий какие-либо действия над городами из репозитория.
  *
@@ -45,9 +48,10 @@ public class CityService {
     }
 
     /**
-     * Преобразование в массив списка городов, вывод индекса и числа жителей самого густонаселенного города
+     * Преобразование в массив списка городов, поиск самого густонаселенного города путем перебора,
+     * вывод индекса и населения города.
      */
-    public void printMostPopulousCityIndexAndPopulation() {
+    public void printHighestPopulationBruteForce() {
         int maxInd = 0, maxPopulation = 0;
 
         City[] citiesArray = cityRepository.getCities().toArray(new City[0]); //получаем список и преобразуем в массив
@@ -59,6 +63,122 @@ public class CityService {
                 maxInd = i;
             }
 
-        System.out.println("[" + maxInd + "] = " + maxPopulation);
+        System.out.println(MessageFormat.format("[{0}] = {1}", maxInd, maxPopulation));
+    }
+
+    /**
+     * Преобразование в массив списка городов, сортировка слиянием по населению в порядке возрастания,
+     * вывод индекса и населения самого густонаселенного города.
+     */
+    public void printHighestPopulationMergeSort() {
+
+        City[] citiesArray = cityRepository.getCities().toArray(new City[0]); //получаем список и преобразуем в массив
+
+        mergeSort(citiesArray);
+
+        int ind = citiesArray.length - 1;
+        System.out.println(MessageFormat.format("[{0}] = {1}", ind, citiesArray[ind].getPopulation()));
+    }
+
+    private void mergeSort(City[] inputArr) {
+        int inputLength = inputArr.length;
+
+        if (inputLength < 2) return;
+
+        int mid = inputLength/2;
+        City[] left = new City[mid];
+        City[] right = new City[inputLength - mid];
+
+        System.arraycopy(inputArr, 0, left, 0, mid);
+        System.arraycopy(inputArr, mid, right, 0, inputLength - mid);
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(inputArr,left,right);
+    }
+
+    private void merge(City[] inputArr, City[] left, City[] right){
+        int leftLength = left.length;
+        int rightLength = right.length;
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < leftLength && j < rightLength){
+            if (left[i].getPopulation() < right[j].getPopulation()){
+                inputArr[k] = left[i];
+                i++;
+            }
+            else {
+                inputArr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < leftLength) {
+            inputArr[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < rightLength) {
+            inputArr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    /**
+     * Преобразование в массив списка городов, быстрая сортировка по населению в порядке возрастания,
+     * вывод индекса и населения самого густонаселенного города.
+     */
+    public void printHighestPopulationQuickSort() {
+
+        City[] citiesArray = cityRepository.getCities().toArray(new City[0]); //получаем список и преобразуем в массив
+
+        quickSort(citiesArray);
+
+        int ind = citiesArray.length - 1;
+        System.out.println(MessageFormat.format("[{0}] = {1}", ind, citiesArray[ind].getPopulation()));
+    }
+
+    private void quickSort(City[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private void quickSort (City[] arr, int startInd, int endInd) {
+        if (startInd >= endInd) return;
+
+        int pivotInd = new Random().nextInt(endInd - startInd) + startInd;
+        int pivot = arr[pivotInd].getPopulation();
+
+        swap(arr, pivotInd, endInd);
+
+        int leftPointer = startInd;
+        int rightPointer = endInd;
+
+        while (leftPointer < rightPointer) {
+            while (arr[leftPointer].getPopulation() <= pivot &&
+                    leftPointer < rightPointer){
+                leftPointer++;
+            }
+            while (arr[rightPointer].getPopulation() >= pivot &&
+                    leftPointer < rightPointer) {
+                rightPointer--;
+            }
+            swap(arr, leftPointer,rightPointer);
+        }
+
+        swap(arr,leftPointer,endInd);
+
+        quickSort(arr, startInd,leftPointer - 1);
+        quickSort(arr,leftPointer + 1, endInd);
+    }
+
+    private void swap(City[] arr, int index1, int index2) {
+        City temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
     }
 }
